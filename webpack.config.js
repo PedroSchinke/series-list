0d38f00cd0804f613005710987094b15f1f42eca
Pedro Schinke <pedro.schinke@outlook.com>
2024-08-14 17:17:36 -0300

    added live reload plugin

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+var LiveReloadWebpackPlugin = require('@kooneko/livereload-webpack-plugin');
+
+module.exports = {
+    plugins: [
+      new LiveReloadWebpackPlugin(options)
+    ]
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 /*
  |--------------------------------------------------------------------------
@@ -14,3 +15,6 @@ const mix = require('laravel-mix');
 mix
     .sass('resources/css/app.scss', 'public/css')
     .js('resources/js/app.js', 'public/js')
+    .webpackConfig({
+        plugins: [new LiveReloadPlugin()]
+    });
