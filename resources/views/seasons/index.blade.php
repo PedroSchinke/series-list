8e60ef2a191171c7d160316206dce554dfda16c9
Pedro Schinke <pedro.schinke@outlook.com>
2024-08-15 16:13:01 -0300

    created seasons.index view

---
+++ resources/views/seasons/index.blade.php
@@ -0,0 +1,15 @@
+<x-layout title="Seasons of {!! $series->name !!}">
+
+    <ul class="list-group">
+        @foreach ($seasons as $season)
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                Season {{ $season->number }}
+
+                <div class="badge bg-secondary">
+                    {{ $season->episodes->count() }}
+                </div>
+
+            </li>
+        @endforeach
+    </ul>
+</x-layout> 
