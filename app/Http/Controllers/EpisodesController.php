31f2c484d7429c95bc6639faddc8bc7feca130f1
Pedro Schinke <pedro.schinke@outlook.com>
2024-08-16 17:10:56 -0300

    added mark episode as watched feature

--- app/Http/Controllers/EpisodesController.php
+++ app/Http/Controllers/EpisodesController.php
@@ -10,7 +10,21 @@ class EpisodesController extends Controller
     public function index(Season $season)
     {
         $episodes = $season->episodes;
+        $series = $season->series;
 
-        return view('episodes.index', compact('episodes', 'season'));
+        return view('episodes.index', compact('episodes', 'season', 'series'));
+    }
+
+    public function update(Season $season, Request $request)
+    {
+        $episodes = $season->episodes()->whereIn('id', $request->episodes)->get();
+
+        foreach ($episodes as $episode) {
+            $episode->watched = true;
+            $episode->save();
+        }
+
+        return redirect()->route('seasons.index', ['series' => $season->series_id])
+            ->with('message.success', "Episodes marked as watched successfully!");
     }
 }
--- resources/views/episodes/index.blade.php
+++ resources/views/episodes/index.blade.php
@@ -1,13 +1,19 @@
-<x-layout title="Episodes of Season {!! $season->number !!}">
+<x-layout title="Episodes of Season {!! $season->number !!} of {!! $series->name !!}">
 
-    <form action="{{ route('episodes.update', $episode->id) }}" method="POST">
+    <form action="{{ route('episodes.update', ['season' => $season->id]) }}" method="POST">
         @csrf
+        {{-- @method('PUT') --}}
         <ul class="list-group">
             @foreach ($episodes as $episode)
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     Episode {{ $episode->number }}
     
-                    <input type="checkbox" name="episodes[]" value="{{ $episode->id }}">
+                    <input 
+                        type="checkbox" 
+                        name="episodes[]" 
+                        value="{{ $episode->id }}"
+                        @if ($episode->watched) checked @endif
+                    />
                 </li>
             @endforeach
         </ul>
