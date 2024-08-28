047d18fcf50614322dc3810cbbbb769fa9e2e9ee
Pedro Schinke <pedro.schinke@outlook.com>
2024-08-28 11:51:21 -0300

    increased validation rules

--- app/Http/Requests/SeriesFormRequest.php
+++ app/Http/Requests/SeriesFormRequest.php
@@ -29,8 +29,9 @@ class SeriesFormRequest extends FormRequest
     {
         return [
             'name' => ['required', 'min:3'],
-            'seasonsQty' => ['required', 'integer'],
-            'episodesPerSeason' => ['required', 'integer']
+            'seasonsQty' => ['required', 'integer', 'min:1', 'max:30'],
+            'episodesPerSeason' => ['required', 'integer', 'min:1', 'max:30'],
+            'cover' => ['file', 'image']
         ];
     }
 
@@ -41,8 +42,14 @@ class SeriesFormRequest extends FormRequest
             'name.min' => "The field 'name' must have at least :min characters",
             'seasonsQty.required' => "The field 'number of seasons' is required",
             'seasonsQty.integer' => "The field 'number of seasons' must be a number",
+            'seasonsQty.min' => "The field 'number of seasons' must be at least :min",
+            'seasonsQty.max' => "The field 'number of seasons' must not exceed :max",
             'episodesPerSeason.required' => "The field 'episodes per season' is required",
             'episodesPerSeason.integer' => "The field 'episodes per season' must be a number",
+            'episodesPerSeason.min' => "The field 'episodes per season' must be at least :min",
+            'episodesPerSeason.max' => "The field 'episodes per season' must not exceed :max",
+            'cover.file' => "The field 'cover' must be a file",
+            'cover.image' => "The 'cover' file must be an image"
         ];
     }
 }
