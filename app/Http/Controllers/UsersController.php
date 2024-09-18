5d343892eb9d46e789c0f3ec039a375eb4ca95f8
Pedro Schinke <pedro.schinke@outlook.com>
2024-09-18 11:06:36 -0300

    extended Controller in UserController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-class UsersController
+class UsersController extends Controller
 {
     public function create()
     {
